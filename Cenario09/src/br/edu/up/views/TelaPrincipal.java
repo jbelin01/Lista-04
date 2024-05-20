83b2b5a076214326d97394b6afcc8ad06baef784
luishf2014 <luishgamer2237@gmail.com>
2024-05-20 17:41:47 -0300

    Add Menu

---
+++ Cenario09/src/br/edu/up/views/TelaPrincipal.java
@@ -0,0 +1,44 @@
+package br.edu.up.views;
+import java.util.Scanner;
+
+public class TelaPrincipal {
+    public void mostrar() {
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            mostrarMenu();
+            String opcao = scanner.nextLine();
+
+            switch (opcao) {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+
+                    break;
+
+                case "4":
+                    System.out.println("Saindo do sistema...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+
+        }
+
+    }
+
+    public void mostrarMenu() {
+        System.out.println("** Bem-Vindo ao  **");
+        System.out.println("1. ");
+        System.out.println("2. ");
+        System.out.println("3. ");
+        System.out.println("4. ");
+        System.out.println("7. Sair");
+        System.out.print("Escolha uma opção: ");
+    }
+}
